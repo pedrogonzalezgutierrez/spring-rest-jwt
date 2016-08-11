02fd22c8ae472db252b3b7d942b9bb096caa0792
pEDROLA <pgongut@yahoo.com>
2016-08-11 22:06:32 +0100

    Refactor

--- src/main/java/com/kiesoft/rest/controller/PublicContentController.java
+++ src/main/java/com/kiesoft/rest/controller/PublicContentController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/public")
+@RequestMapping(value = "/")
 public class PublicContentController {
 	
 	@RequestMapping(method = RequestMethod.GET)
